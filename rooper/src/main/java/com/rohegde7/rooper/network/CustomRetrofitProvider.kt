9cd8c24bb16826dbbbc7ca0d20c9edda83c59cfa
rohegde7 <hegde.rohit7@gmail.com>
2020-05-11 12:21:50 +0530

    interceotors and okhttp removed

--- rooper/src/main/java/com/rohegde7/rooper/network/CustomRetrofitProvider.kt
+++ rooper/src/main/java/com/rohegde7/rooper/network/CustomRetrofitProvider.kt
@@ -1,9 +1,7 @@
 package com.rohegde7.rooper.network
 
 import com.google.gson.GsonBuilder
-import com.rohegde7.rooper.ignore.interceptor.LoggerInterceptor
 import io.reactivex.schedulers.Schedulers
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -17,17 +15,17 @@ object CustomRetrofitProvider {
     fun getRetrofit(baseUrl: String): Retrofit {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
-            .client(getMyHttpClient())
+//            .client(getMyHttpClient())
             .addConverterFactory(GsonConverterFactory.create(gson))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
             .build()
     }
 
-    private fun getMyHttpClient(): OkHttpClient {
+    /*private fun getMyHttpClient(): OkHttpClient {
         val builder = OkHttpClient().newBuilder()
 //            .addNetworkInterceptor(CachingInterceptor())
 //            .addNetworkInterceptor(ForceCacheInterceptor())
             .addNetworkInterceptor(LoggerInterceptor())
         return builder.build()
-    }
+    }*/
 }
